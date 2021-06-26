7fe379295f7787e3ff931925292f400ef1ac5632
20880115 <you@example.com>
2021-06-26 22:54:06 +0700

    2

--- Calculator App/src/Calculator.java
+++ Calculator App/src/Calculator.java
@@ -1,3 +1,5 @@
+//14:52
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -9,6 +11,7 @@ public class Calculator implements ActionListener {
 	JFrame frame;
 	JTextField textfield;
 	JButton[] numberButtons = new JButton[10];
+	JButton[] functionButtons = new JButton[8];
 	JButton addButton, subButton, mulButton, divButton;
 	JButton decButton, equButton, delButton, clrButton;
 	JPanel panel;
@@ -24,7 +27,34 @@ public class Calculator implements ActionListener {
 		frame.setSize(420,550);
 		frame.setLayout(null);
 		
+		// create check box
+		textfield = new JTextField();
+		textfield.setBounds(50, 50, 300, 50);
+		textfield.setFont(myFont);
+		textfield.setEditable(false); // cant add text
 		
+		addButton = new JButton("+");
+		subButton = new JButton("-");
+		mulButton = new JButton("*");
+		divButton = new JButton("/");
+		decButton = new JButton(".");
+		equButton = new JButton("=");
+		delButton = new JButton("Del");
+		clrButton = new JButton("Clr");
+		
+		
+		functionButtons[0]= addButton;
+		functionButtons[1]= subButton;
+		functionButtons[2]= mulButton;
+		functionButtons[3]= divButton;
+		functionButtons[4]= decButton;
+		functionButtons[5]= equButton;
+		functionButtons[6]= delButton;
+		functionButtons[7]= clrButton;
+		
+		
+		//call things out
+		frame.add(textfield);
 		frame.setVisible(true);
 	}
 	
