8e2f02c2a36b716cf1289004d8a70c2ef3b57478
20880115 <you@example.com>
2021-06-26 22:37:51 +0700

    2

--- Calculator App/src/Calculator.java
+++ Calculator App/src/Calculator.java
@@ -5,12 +5,32 @@ import java.awt.event.*;
 
 public class Calculator implements ActionListener {
 	
+	//things we need declare
+	JFrame frame;
+	JTextField textfield;
+	JButton[] numberButtons = new JButton[10];
+	JButton addButton, subButton, mulButton, divButton;
+	JButton decButton, equButton, delButton, clrButton;
+	JPanel panel;
+	
+	Font myFont = new Font("Ink Free", Font.BOLD,30);
+	
+	double num1 =0, num2 =0, result=0;
+	char operator;
+	
 	Calculator() {
+		frame = new JFrame("Calculator");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(420,550);
+		frame.setLayout(null);
+		
+		
+		frame.setVisible(true);
 	}
 	
 	
 	public static void main(String[] args) {
-		
+		Calculator calc = new Calculator();
 		
 	}
 
